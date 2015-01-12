2bfda52155aaa26cc69c7c3b12804b6214baf46d
Shane Argo <shane@shaneargo.net>
2015-01-12 12:06:57 +1000

    Removed unneeded import.

--- src/test/java/com/alltheducks/stripes/rest/RestfulExceptionHandlerTest.java
+++ src/test/java/com/alltheducks/stripes/rest/RestfulExceptionHandlerTest.java
@@ -14,7 +14,6 @@ import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
 public class RestfulExceptionHandlerTest {
 
